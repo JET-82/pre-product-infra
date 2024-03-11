9406e102019b52e4595316768ede1684a207c0b1
gdtknight <gdtknight@naver.com>
2024-03-11 19:49:13 +0900

    add variables.tf

---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "organization" {
+  type        = string
+  description = "Terraform cloud organization"
+  default     = "hyungwook"
+}
+
+variable "project" {
+  type        = string
+  description = "Terraform cloud project"
+  default     = "pre-product"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "ap-northeast-2"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "AWS vpc name"
+  default    = "pre-product"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "AWS vpc cidr"
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "AWS private subnets cidr list"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "AWS public subnets cidr array"
+  default     = ["10.0.10.0/4", "10.0.12.0/24", "10.0.13.0/24"]
+}
+
+variable "eks_cluster_name" {
+  default = "pre-eks"
+}
